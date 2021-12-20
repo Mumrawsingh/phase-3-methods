d5c045d538b1c424f859bd178bf277f06bb7e9fb
Matthew Umrawsingh <matthew.umrawsingh@gmail.com>
2021-12-20 11:08:15 -0500

    commit tree b8913cd09bdef2c30dc45805fca9526e750459e5

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
-# Your code here!
+#1
+
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+
+end
+
+greet
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+greet_with_default "Naureen"
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(number)
+    return nil unless number.class == Integer
+
+    number/2
+
+end
+
+
